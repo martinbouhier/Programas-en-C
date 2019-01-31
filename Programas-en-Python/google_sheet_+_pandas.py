a6e85cb44b345c6a42910c102f30eaef84b3d785
Martin Bouhier <41752956+martinbouhier@users.noreply.github.com>
2019-01-31 18:24:03 -0300

    Update

--- Programas-en-Python/google_sheet_+_pandas.py
+++ Programas-en-Python/google_sheet_+_pandas.py
@@ -1,16 +1,40 @@
-import pandas as pd
-import pygsheets
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+	Primero que nada debemos tener creada la app necesaria que nos dará el .json
+con los valores correspondientes para poder loguearnos en google
+	Algo muy importante es que se deben habilitar dentro de las credenciales
+de la app creada tanto el api de google_sheets como el de google_drive.
+	El json deberá ser extraido desde la app y tedrá los siguientes campos:
+	{
+	  "type": "service_account",
+	  "project_id": "drive-230113",
+	  "private_key_id": "",
+	  "private_key": "",
+	  "client_email": "",
+	  "client_id": "",
+	  "auth_uri": "",
+	  "token_uri": "",
+	  "auth_provider_x509_cert_url": "",
+	  "client_x509_cert_url": ""
+	}
+    Además debemos crear el documeto de excel_sheet en google,
+compartirlo con el mail que veremos en "client_email": "" (dentro del json)
+"""
+import pandas as pd  # pandas 0.23.4
+import pygsheets  # pygsheets 2.0.0
 
 class Google:
     def __init__(self, service_file):
         self.service_file = service_file
     def update_sheet(self, file_name, csv_name, sheet, clean_start_cel, clean_end_cel, df_fila, df_columna):
         gc = pygsheets.authorize(service_file=self.service_file)
-        sh = gc.open(self.file_name)
-        wks = sh[self.sheet]
-        wks.clear(start=self.clean_start_cel, end=self.clean_end_cel)
-        df = pd.read_csv(self.csv_name)
-        wks.set_dataframe(df,(self.df_fila, self.df_columna))
+        sh = gc.open(file_name)
+        wks = sh[sheet]
+        wks.clear(start=clean_start_cel, end=clean_end_cel)
+        df = pd.read_csv(csv_name)
+        wks.set_dataframe(df,(df_fila, df_columna))
 
 
 # Ingreso de valores para la clase y la función
